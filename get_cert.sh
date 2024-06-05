ebfbc69a7e3fd1b016b8d4f898db1ca8d278586a
Scott <scott@Nemo.local>
2024-06-05 09:53:30 +0100

    push to master

--- get_cert.sh
+++ get_cert.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
 # Get
-#echo | openssl s_client -showcerts -servername svn-ccsm-models.cgd.ucar.edu -connect svn-ccsm-models.cgd.ucar.edu:443 2>/dev/null | openssl x509 -inform pem -out svn-ccsm-cert.pem
 echo | openssl s_client -showcerts -servername svn-ccsm-models.cgd.ucar.edu -connect svn-ccsm-models.cgd.ucar.edu:443 2>/dev/null | openssl x509 -inform pem -out svn-ccsm-cert.crt
 
 # Verify
-#openssl x509 -in svn-ccsm-cert.pem -text -noout
 openssl x509 -in svn-ccsm-cert.crt -text -noout
 
 # Get Intermediate
