6ea2b420f59465964ebe7bd53fc423b279d67af5
Scott <nemo@Scotts-Mac-mini.lan>
2024-06-11 09:21:17 +0100

    netcdf-fortran should install

--- cam/run.sh
+++ cam/run.sh
@@ -2,5 +2,6 @@
 
 #docker run -it cesm /bin/bash
 #docker run --hostname ubuntu-latest -it cam /bin/bash
-docker run -it cam /bin/bash
+#docker run -it cam /bin/bash
+docker run -it -v "$(pwd)/logs:/logs" cam /bin/bash
 
