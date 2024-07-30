355debf355591650ce894e303e9965b7ab2bc56d
dory <scott@Dory.local>
2024-07-30 07:54:33 -0400

    changed sim settings

--- cesm/run.sh
+++ cesm/run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 #docker run -it cesm /bin/bash
-docker run -it -v $HOME/Documents/code/cesm_inputs:/root/cesm cesm /bin/bash
+#docker run -it -v $HOME/Documents/code/cesm_inputs:/root/cesm cesm /bin/bash
+docker run -it --cpus="8" -v $HOME/Documents/thor_shit/cesm_inputs:/root/cesm cesm /bin/bash
 
