455877af401886b8b129975846047d9539acba6d
Scott <none@gmail.com>
2024-11-19 18:26:52 -0500

    stupid container had hpcx installed, don't want that

--- cesm/run.sh
+++ cesm/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 #docker run -it cesm /bin/bash
-docker run -it -v $HOME/Documents/cesm_inputs:/root/cesm cesm /bin/bash
+docker run --gpus all --ipc=host --ulimit memlock=-1 -it -v $HOME/Documents/cesm_inputs:/root/cesm cesm /bin/bash
 
